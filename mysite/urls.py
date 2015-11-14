37a1797b09769435817b4608af5dd0d8a69f1baf
makao <2016333261@qq.com>
2015-11-14 23:36:49 +0800

    abandon angular, set up the main front end frame

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from yuqq import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$',include('yuqq.urls')),
+    url(r'^$',views.index,name='index'),
+    url(r'^services/$',views.services,name='services'),
+    url(r'^aboutme/$',views.aboutme,name='aboutme'),
+    url(r'^message/$',views.message,name='message'),
+    url(r'^products/$',views.products,name='products'),
+    url(r'^knowledge/$',views.knowledge,name='knowledge'),
 ]
--- yuqq/views.py
+++ yuqq/views.py
@@ -4,4 +4,19 @@ from django.shortcuts import render_to_response
 from mysite import settings
 # Create your views here.
 def index(request):
-    return render_to_response('public/index.html',{'title':settings.STATIC_URL})
+    return render_to_response('public/index.html',{'title':'鱼圈圈'})
+
+def aboutme(request):
+    return render_to_response('public/aboutme.html',{'info':'我们是鱼圈圈'})
+
+def services(request):
+    return render_to_response('public/services.html',{'services':'洗鱼缸'})
+
+def message(request):
+    return render_to_response('public/message.html',{'message':'留言'})
+
+def products(request):
+    return render_to_response('public/products.html',{'products':'热销商品'})
+
+def knowledge(request):
+    return render_to_response('public/knowledge.html',{'knowledge':'专业知识'})
