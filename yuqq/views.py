73f4406374966d5674e39ef0d9b86e9243e2f6b8
makao <2016333261@qq.com>
2015-11-14 16:53:53 +0800

    add angular.min.js

--- yuqq/views.py
+++ yuqq/views.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-
+from mysite import settings
 # Create your views here.
 def index(request):
-    return render_to_response('index.html',{'title':"yuqq"})
+    return render_to_response('public/index.html',{'title':settings.STATIC_URL})
