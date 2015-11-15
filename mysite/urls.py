a780dbbc72ccac54e8eb7b3c8f1c9b79d8bdedda
makao <2016333261@qq.com>
2015-11-15 16:37:01 +0800

    请求图片成功，但不能加载

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from yuqq import views
+from . import settings
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$',views.index,name='index'),
@@ -24,4 +25,5 @@ urlpatterns = [
     url(r'^message/$',views.message,name='message'),
     url(r'^products/$',views.products,name='products'),
     url(r'^knowledge/$',views.knowledge,name='knowledge'),
+    url(r'^media/(?P<path>.*)', 'django.views.static.serve', {'document_root': '/Users/makao/Yun/Workspace/django/yuqq/yuqq/static/public/img'}),
 ]
